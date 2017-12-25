4fcce784fcc251774969f4c7d7e7c7770897d1f5
Andrew Bellinger <gravityab@gmail.com>
2017-12-24 22:09:34 -0800

    Added parabolic turn transition movement
    * Speed is based on distance from the center to give
      it that fast to slow to fast again feel.
    * Fixed backdrop image material.

--- Assets/Scripts/Mission.cs
+++ Assets/Scripts/Mission.cs
@@ -177,11 +177,13 @@ public class Mission : MonoBehaviour
             // While that distance is greater than a very small amount (Epsilon, almost zero):
             while (backdropRect.transform.position.x != endBackdrop.x)
             {
+                float speed = Mathf.Abs(Vector2.Distance(textRect.transform.position, new Vector2(canvasRect.rect.width / 2, textRect.transform.position.y))) * 8 + 1;
+
                 // Find a new position proportionally closer to the end, based on the moveTime
                 Vector2 newTextPostion = Vector2.MoveTowards(
-                    textRect.transform.position, endText, (1f / .001f) * Time.deltaTime);
+                    textRect.transform.position, endText, speed * Time.deltaTime);
                 Vector2 newBackdropPostion = Vector2.MoveTowards(
-                    backdropRect.transform.position, endBackdrop, (1f / .001f) * Time.deltaTime);
+                    backdropRect.transform.position, endBackdrop, speed * Time.deltaTime);
 
                 // Call MovePosition on attached Rigidbody2D and move it to the calculated position.
                 textRect.transform.position = newTextPostion;
