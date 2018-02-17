81cce2f99542409b8f10c1a3bea2efa20e860cf5
cladjevardi <cladjevardi@gmail.com>
2018-02-16 16:00:19 -0800

    Created a UiButton function that creates buttons
    * The buttons don't do anything currently

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -9,8 +9,14 @@ public class Menu : MonoBehaviour
     /// <summary>The canvas for UI.</summary>
     private GameObject canvas = null;
 
-    /// <summary>The sideBar for button UI.</summary>
-    private GameObject menuTest = null;
+    /// <summary>The menu background for menu UI.</summary>
+    private GameObject menuBackground = null;
+
+    /// <summary>The menu title for the menu UI.</summary>
+    private GameObject menuTitle = null;
+
+    /// <summary>The playDemoButton for the menu UI.</summary>
+    private GameObject playDemoButton = null;
 
     // Use this for initialization
     void Awake ()
@@ -22,22 +28,63 @@ public class Menu : MonoBehaviour
         can.renderMode = RenderMode.ScreenSpaceOverlay;
         RectTransform canvasRect = canvas.GetComponent<RectTransform>();
 
-        // Create the sidebar to contain the buttons.
-        menuTest = new GameObject("Menu");
-        menuTest.transform.SetParent(canvas.gameObject.transform);
-        Image menuTestImage = menuTest.AddComponent<Image>();
-        menuTestImage.material = GameManager.instance.uiMaterials[5];
-        menuTestImage.transform.position = new Vector3(0, 0, -0.1f);
-        RectTransform menuTestRect = menuTestImage.GetComponent<RectTransform>();
-        menuTestRect.anchorMin = new Vector2(1, 0);
-        menuTestRect.anchorMax = new Vector2(1, 1);
-        menuTestRect.anchoredPosition = new Vector2(-360, 0);
-        menuTestRect.sizeDelta = new Vector2(800, 0);
+        // Create the Menu Background to contain the buttons.
+        menuBackground = new GameObject("Menu Background");
+        menuBackground.transform.SetParent(canvas.gameObject.transform);
+        Image menuBackgroundImage = menuBackground.AddComponent<Image>();
+        menuBackgroundImage.material = GameManager.instance.uiMaterials[5];
+        RectTransform menuBackgroundRect = menuBackgroundImage.GetComponent<RectTransform>();
+        menuBackgroundRect.anchoredPosition = new Vector2(0, 0);
+        menuBackgroundRect.sizeDelta = new Vector2(768, 768);
+
+        // Create the Menu Title.
+        menuTitle = new GameObject("menuTitle");
+        menuTitle.transform.SetParent(canvas.gameObject.transform);
+        Image menuTitleImage = menuTitle.AddComponent<Image>();
+        menuTitleImage.material = GameManager.instance.uiMaterials[1];
+        RectTransform menuTitleRect = menuTitleImage.GetComponent<RectTransform>();
+        menuTitleRect.anchoredPosition = new Vector2(0, 120);
+        menuTitleRect.sizeDelta = new Vector2(557, 191);
+
+        /*
+        // Create the Play Demo Button.
+        playDemoButton = new GameObject("Play Demo playDemoButton");
+        playDemoButton.AddComponent<Button>();
+        // TODO: make playDemo play the demo
+        //playDemoButton.GetComponent<Button>().onClick.AddListener(playDemo);
+        playDemoButton.transform.SetParent(canvas.gameObject.transform);
+        Image playDemoButtonImage = playDemoButton.AddComponent<Image>();
+        playDemoButtonImage.material = GameManager.instance.uiMaterials[4];
+        RectTransform playDemoButtonRect = playDemoButtonImage.GetComponent<RectTransform>();
+        playDemoButtonRect.anchoredPosition = new Vector2(-170, -130);
+        playDemoButtonRect.sizeDelta = new Vector2(320, 215);
+        */
+
+        // Create the Play Demo Button
+        UiButton(GameManager.instance.uiMaterials[4], 
+            new Vector2(320, 215), new Vector2(-170, -130), canvas);
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    /// <summary>Create a UI button</summary>
+    /// <param name="material">The material for the button.</param>
+    /// <param name="size">The Vector2 width and height of the button.</param>
+    /// <param name="position">The Vector2 x and y anchor position of the button.</param>
+    /// <param name="canvas">The canvas object to create the button on.</param>
+    /// <returns>Returns whether the update loop should process the current frame as handled.</returns>
+    public static UnityEngine.UI.Button UiButton(Material material, 
+        Vector2 size, Vector2 position, GameObject canvas)
     {
-		
-	}
+        GameObject gameObject = new GameObject("Textured button (" + material.name + ")");
+
+        Image image = gameObject.AddComponent<Image>();
+        image.material = material;
+
+        UnityEngine.UI.Button button = gameObject.AddComponent<UnityEngine.UI.Button>();
+        gameObject.transform.SetParent(canvas.transform, false);
+
+        image.rectTransform.sizeDelta = size;
+        image.rectTransform.anchoredPosition = position;
+
+        return button;
+    }
 }
